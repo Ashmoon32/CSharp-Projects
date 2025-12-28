9eb5fa04ba4b59d7a85557df782fed15997441d0
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-12-28 20:24:20 +0630

    Initial Commit

---
+++ Console-Based-RPG/Console-Based-RPG/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TextBasedRPG
+{
+    class Start
+    {
+        // For Player
+        string playerName;
+        int playerHealth = 100;
+        int playerEnergy = 100;
+        bool isPlayerAlive = true;
+
+        // For Normal Monster
+        string monsterName = "Dragon";
+        int monsterHealth = 50;
+        bool isMonsterAlive = true;
+        int monsterAttackDamage = 5;
+
+        // For Elite Monster
+        string eliteMonsterName = "Chaos Warlord";
+        int eliteMonsterHealth = 200;
+        int eliteMonsterAttackDamage = 20;
+        int eliteMonsterUltimateDamage = 50;
+        bool isEliteMonsterAlive = true;
+
+        // For Final Monster Boss
+        string bossMonsterName = "King of Immortality";
+        int bossMonsterHealth = 500;
+        int bossMonsterNormalPhysicalAttackDamage = 100;
+        int bossMonsterMagicAttackDamage = 150;
+        int bossMonsterUltimateAttackDamage = 300;
+        bool isBossMonsterAlive = true;
+
+
+
+
+    }
+}
