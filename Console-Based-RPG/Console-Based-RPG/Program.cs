a7d1156f1be41ab3309e82ec0d7ddd3e181b45fe
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-12-28 23:07:26 +0630

    add dragon monster section(just player part)

--- Console-Based-RPG/Console-Based-RPG/Program.cs
+++ Console-Based-RPG/Console-Based-RPG/Program.cs
@@ -10,7 +10,14 @@ namespace TextBasedRPG
             // For Player
             string playerName;
             int playerHealth = 100;
-            int playerEnergy = 100;
+            int playerMana = 100;
+            int playerPhysicalAttackDamage = 10;
+            int playerMagicDamage = 20;
+            int playerUltimateDamage = 40;
+            int playerManaCostPerPhysicalAttack = 5;
+            int playerManaCostPerMagicAttack = 10;
+            int playerManaCostPerUlitmateAttack = 30;
+            int playerManaCostPerDefend = 5;
             bool isPlayerAlive = true;
 
             // For Normal Monster
@@ -32,11 +39,15 @@ namespace TextBasedRPG
             int bossMonsterNormalPhysicalAttackDamage = 100;
             int bossMonsterMagicAttackDamage = 150;
             int bossMonsterUltimateAttackDamage = 300;
+            int bossSpeicalSpell = bossMonsterHealth += 100;
             bool isBossMonsterAlive = true;
 
             // For Player Choice
             string initialChoice;
             string PlayerChoice;
+
+            // Others
+            int turn = 0;
 
 
             // Game introduction and Player Input           
@@ -53,16 +64,77 @@ namespace TextBasedRPG
                     bool isValid = false;
                     do
                     {
-                        
-
                         Console.Write("Please enter your character name: ");
                         playerName = Console.ReadLine();
 
+                        // Force user only to enter alphabetic characters
                         if (Regex.IsMatch(playerName, @"^[a-zA-Z]+$"))
                         {
                             isValid = true;
                             Console.WriteLine($"Welcome Legend {playerName}, your journey has begun! ");
+                            Console.WriteLine("========================================================");
 
+                            // Start the actual game play here
+
+                            // Dragon Monster Section - Start 
+
+                            Console.WriteLine("Ohh {0}! You see the monster called {1}. What would you do?",playerName, monsterName);
+
+                            while(isPlayerAlive && isMonsterAlive)
+                            {
+                                // We will make simple game logic, player turn is even number and monster is odd.
+                                Console.WriteLine(" 1. Attack {0} 2. Defend {0} 3. Magic Attack {0} 4. Ultimate",Environment.NewLine);
+                                Console.WriteLine("Enter your choice (default is 1): ");
+                                if(turn % 2 == 0)
+                                {
+                                    
+                                    PlayerChoice = Console.ReadLine();
+                                    switch (PlayerChoice)
+                                    {
+                                        case "1":
+                                            Console.WriteLine($"You attack the {monsterName} with physical attack!");
+                                            monsterHealth -= playerPhysicalAttackDamage;
+                                            playerMana -= playerManaCostPerPhysicalAttack;
+                                            break;
+
+                                        case "2":
+                                            Console.WriteLine($"You defend from the {monsterName}'s attack! No monster damage is effected.");
+                                            playerMana -= playerManaCostPerDefend;
+                                            break;
+
+                                        case "3":
+                                            Console.WriteLine($"You attack the {monsterName} with magic attack!");
+                                            monsterHealth -= playerMagicDamage;
+                                            playerMana -= playerManaCostPerMagicAttack;
+                                            break;
+
+                                        case "4":
+                                            Console.WriteLine($"You attack the {monsterName} with your ultimate attack!!");
+                                            monsterHealth -= playerUltimateDamage;
+                                            playerMana -= playerManaCostPerUlitmateAttack;
+                                            break;
+
+                                        default:
+                                            Console.WriteLine($"You attack the {monsterName} with physical attack!");
+                                            monsterHealth -= playerPhysicalAttackDamage;
+                                            playerMana -= playerManaCostPerPhysicalAttack;
+                                            break;
+                                    }
+                                    
+                                    if(monsterHealth <= 0)
+                                    {
+                                        Console.WriteLine($"{monsterName} is died. You win.");
+                                        isMonsterAlive = false;
+                                    } else if(playerHealth <= 0)
+                                    {
+                                        Console.WriteLine($"You have been killed by {monsterName}. Game Over!");
+                                        isPlayerAlive = false;
+                                    } else
+                                    {
+                                        Console.WriteLine(" Player Health : {0} {3} Player Mana : {1} {3} Monster Health : {2}", playerHealth, playerMana, monsterHealth, Environment.NewLine);
+                                    }
+                                }
+                            }
                         } else
                         {
                             Console.WriteLine("Invalid Character Name! Please use only letters(a to z, A to Z).");
@@ -83,15 +155,7 @@ namespace TextBasedRPG
             } finally
             {
                 Console.WriteLine("Program End.");
-            }
-
-            
-
+            }         
         }
-
-
-
-
-
     }
 }
