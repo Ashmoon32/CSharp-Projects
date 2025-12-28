80bc6eefb66ac3f8d87c20d588ec31e527af10ed
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-12-28 20:28:16 +0630

    -

--- Console-Based-RPG/Console-Based-RPG/Program.cs
+++ Console-Based-RPG/Console-Based-RPG/Program.cs
@@ -2,34 +2,41 @@
 
 namespace TextBasedRPG
 {
-    class Start
+    class Program
     {
-        // For Player
-        string playerName;
-        int playerHealth = 100;
-        int playerEnergy = 100;
-        bool isPlayerAlive = true;
+        static void Main(string[] args)
+        {
+            // For Player
+            string playerName;
+            int playerHealth = 100;
+            int playerEnergy = 100;
+            bool isPlayerAlive = true;
 
-        // For Normal Monster
-        string monsterName = "Dragon";
-        int monsterHealth = 50;
-        bool isMonsterAlive = true;
-        int monsterAttackDamage = 5;
+            // For Normal Monster
+            string monsterName = "Dragon";
+            int monsterHealth = 50;
+            bool isMonsterAlive = true;
+            int monsterAttackDamage = 5;
 
-        // For Elite Monster
-        string eliteMonsterName = "Chaos Warlord";
-        int eliteMonsterHealth = 200;
-        int eliteMonsterAttackDamage = 20;
-        int eliteMonsterUltimateDamage = 50;
-        bool isEliteMonsterAlive = true;
+            // For Elite Monster
+            string eliteMonsterName = "Chaos Warlord";
+            int eliteMonsterHealth = 200;
+            int eliteMonsterAttackDamage = 20;
+            int eliteMonsterUltimateDamage = 50;
+            bool isEliteMonsterAlive = true;
 
-        // For Final Monster Boss
-        string bossMonsterName = "King of Immortality";
-        int bossMonsterHealth = 500;
-        int bossMonsterNormalPhysicalAttackDamage = 100;
-        int bossMonsterMagicAttackDamage = 150;
-        int bossMonsterUltimateAttackDamage = 300;
-        bool isBossMonsterAlive = true;
+            // For Final Monster Boss
+            string bossMonsterName = "King of Immortality";
+            int bossMonsterHealth = 500;
+            int bossMonsterNormalPhysicalAttackDamage = 100;
+            int bossMonsterMagicAttackDamage = 150;
+            int bossMonsterUltimateAttackDamage = 300;
+            bool isBossMonsterAlive = true;
+
+            // Game introduction and Player Input
+
+        }
+
 
 
 
