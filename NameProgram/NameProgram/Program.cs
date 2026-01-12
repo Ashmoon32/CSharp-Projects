588445450377cfebad1f5ea4f498f0e26b0ee92d
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-01-12 22:31:49 +0630

    -

---
+++ NameProgram/NameProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Remoting.Services;
+
+namespace NameProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter you first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Your name is : {0} {1}",firstName,lastName);
+
+            Console.ReadKey();
+        }
+    }
+}
