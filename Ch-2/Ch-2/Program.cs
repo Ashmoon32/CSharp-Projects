07ab2d61d21228a28dd4d9ad5325a521dc345f56
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-07 20:27:47 +0630

    -

---
+++ Ch-2/Ch-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 45;
+        Console.WriteLine(i);
+        float f = 3.14f;
+        Console.WriteLine(f);
+        char c = 'A';
+        Console.WriteLine(c);
+        bool isTrue = true;
+        Console.WriteLine(isTrue);
+        Console.WriteLine();
+        string stu_name = "Ashmoon";
+        int stu_age = 21;
+        char grade = 'S';
+        Console.WriteLine("Name : " + stu_name);
+        Console.WriteLine("Age : " + stu_age);
+        Console.WriteLine("Grade : " + grade);
+    }
+}
