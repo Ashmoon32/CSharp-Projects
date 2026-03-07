89bdc1f644dc26c194fc6a3398d590a019009120
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-07 22:15:27 +0630

    -

--- Ch-2/Ch-2/Program.cs
+++ Ch-2/Ch-2/Program.cs
@@ -12,12 +12,34 @@ class Program
         Console.WriteLine(c);
         bool isTrue = true;
         Console.WriteLine(isTrue);
+
+
         Console.WriteLine();
+
+
         string stu_name = "Ashmoon";
         int stu_age = 21;
         char grade = 'S';
         Console.WriteLine("Name : " + stu_name);
         Console.WriteLine("Age : " + stu_age);
         Console.WriteLine("Grade : " + grade);
+
+        Console.WriteLine();
+
+        long l = 123345L;
+        byte b = 123;
+        Console.WriteLine("Long: " + l);
+        Console.WriteLine("Byte: " + b);
+
+        Console.WriteLine();
+
+        int A = 10, B = 3;
+        Console.WriteLine("Addition: " + (A + B));
+        Console.WriteLine("Subtraction: " + (A - B));
+        Console.WriteLine("Multiplication: " + (A * B));
+        Console.WriteLine("Division: " + (A / B));
+        Console.WriteLine("Remainder: " + (A % B));
+
+
     }
 }
