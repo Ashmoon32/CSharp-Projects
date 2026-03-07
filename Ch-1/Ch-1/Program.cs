fb30ab6283b643e00fa016f5ae230bfeb0313ec2
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-07 13:13:01 +0630

    -

---
+++ Ch-1/Ch-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ch_1;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise-1:");
+            exe1.Run();
+        }
+    }
+}
