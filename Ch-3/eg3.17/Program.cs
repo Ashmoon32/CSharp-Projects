471d44f1c9f5b27af17ccdb84747a0c24033ff0e
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-10 13:10:47 +0630

    -

---
+++ Ch-3/eg3.17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandlingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\AshMoon\\sample.txt";
+
+            StreamWriter Writer = new StreamWriter(path);
+            Writer.WriteLine(Console.ReadLine());
+            Writer.WriteLine("This file is created by using StreamWriter!");
+            Writer.Close();
+
+            Console.WriteLine("File created and data wrote successfully!");
+
+            StreamReader Reader = new StreamReader(path);
+            string content = Reader.ReadToEnd();
+            Reader.Close();
+
+            Console.WriteLine("Contents of the file: ");
+            Console.WriteLine(content);
+        }
+    }
+}
