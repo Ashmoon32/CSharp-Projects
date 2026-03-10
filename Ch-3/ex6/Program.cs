ce2e25efa7c89ff0baff038eb03df339a7a4d038
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-10 17:54:31 +0630

    ex6

---
+++ Ch-3/ex6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Square of numbers from 1 to 10:");
+        for (int i = 1; i <= 10; i++)
+        {
+            int square = i * i;
+            Console.WriteLine("Number: " + i + ",Square: " + square);
+        }
+    }
+}
