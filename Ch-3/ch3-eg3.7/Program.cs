34d48d65422c8439fe6a0c291f43e1b222c841cc
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-10 12:07:18 +0630

    eg3.7

---
+++ Ch-3/ch3-eg3.7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string isUserDone = "";
+        do
+        {
+            Console.WriteLine("In do/while loop:");
+            Console.Write("Are you done? [yes/no]: ");
+            isUserDone = Console.ReadLine();
+        } while (isUserDone.ToLower() != "yes");
+    }
+}
