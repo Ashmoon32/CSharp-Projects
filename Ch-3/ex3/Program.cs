f062c34e068f4a07b3dabecaed1797d839651ff8
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-10 17:15:22 +0630

    ex3

---
+++ Ch-3/ex3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your percentage: ");
+        int percentage = Convert.ToInt32(Console.ReadLine());
+
+        if (percentage >= 90)
+        {
+            Console.WriteLine("Excellent!");
+        } else if (percentage >= 75)
+        {
+            Console.WriteLine("Good!");
+        } else if (percentage >= 50)
+        {
+            Console.WriteLine("Average!");
+        } else
+        {
+            Console.WriteLine("Poor!");
+        }
+    }
+}
