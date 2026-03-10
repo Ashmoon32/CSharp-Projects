05f2b65190353adf05542759ce82aa0c13fc847e
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-10 13:46:17 +0630

    ex2

---
+++ Ch-3/ex2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int largest;
+        Console.Write("Enter first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter first number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter first number: ");
+        int num3 = int.Parse(Console.ReadLine());
+
+        if (num1 > num2)
+        {
+            if (num1 > num3)
+            {
+                largest = num1;
+            } else
+            {
+                largest = num3;
+            }
+        } else
+        {
+            if (num2 > num3)
+            {
+                largest = num2;
+            } else
+            {
+                largest = num3;
+            }
+        }
+
+        Console.WriteLine("Largest number is: " + largest);
+    }
+    
+
+}
