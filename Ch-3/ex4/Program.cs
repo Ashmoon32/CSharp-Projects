475d31c2685319818050352746dd227eab078aa1
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-10 17:24:22 +0630

    ex4

---
+++ Ch-3/ex4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the total purchase amount(KS): ");
+        double amount = double.Parse(Console.ReadLine());
+        double discount;
+
+        if(amount >= 5000)
+        {
+            discount = amount * 0.20;
+        } else if (amount >= 3000 && amount < 5000)
+        {
+            discount = amount * 0.10;
+        } else
+        {
+            discount = 0;
+        }
+
+        double finalAmount = amount - discount;
+        Console.WriteLine("Discount: " + discount + "KS");
+        Console.WriteLine("Final amount to pay: " + finalAmount + "KS");
+    }
+}
