aa892dc7ae9d9ef95c98309a1c488f364445f43b
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-10 12:44:39 +0630

    eg3.15

---
+++ Ch-3/ch3-eg3.13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassObjectExample
+{
+    class Student
+    {
+        public int studentId;
+        public string studentName;
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Student ID: " + studentId);
+            Console.WriteLine("Student Name: " + studentName);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+
+            s1.studentId = 1;
+            s1.studentName = "Ashmoon";
+
+            s1.DisplayDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ch-3/eg3.15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MultilevelInheritance
+{
+    class Person
+    {
+        public string Name;
+        public void ShowName()
+        {
+            Console.WriteLine("Name: " + Name);
+        }
+    }
+
+    class Student : Person
+    {
+        public string StudentId;
+
+        public void ShowStudentId()
+        {
+            Console.WriteLine("Student Id: " + StudentId);
+        }
+    }
+
+    class Graduate : Student
+    {
+        public string Degree;
+
+        public void ShowDegree()
+        {
+            Console.WriteLine("Degree: " + Degree);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graduate g1 = new Graduate();
+
+            g1.Name = "Ashmoon";
+            g1.StudentId = "3GIT-1";
+            g1.Degree = "A.G.T.I Information Technology";
+
+            g1.ShowName();
+            g1.ShowStudentId();
+            g1.ShowDegree();
+        }
+    }
+}
