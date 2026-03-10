35b027ad27ff7cf158be44056ca302e0c2e0c2e3
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-10 11:05:46 +0630

    eg3.2

---
+++ Ch-3/Ch-3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if(number > 0)
+        {
+            Console.WriteLine("The Number is Positive!");
+        } else if (number < 0)
+        {
+            Console.WriteLine("The number is Negative!");
+        } else
+        {
+            Console.WriteLine("The number is Zero!");
+        }
+    }
+}
