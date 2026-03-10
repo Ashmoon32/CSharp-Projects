a77452468848d3976bbdbc0f574c7eaa0d8889d7
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-10 18:23:16 +0630

    ex8

---
+++ Ch-3/ex8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class MathOperation
+{
+    public int CalculateArea(int side)
+    {
+        return side * side;
+    }
+
+    public int CalculateArea(int length, int width)
+    {
+        return length * width;
+    }
+
+    public double CalculateArea(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MathOperation obj = new MathOperation();
+        int squareArea = obj.CalculateArea(5);
+        Console.WriteLine("Square Area: " + squareArea);
+        int rectangleArea = obj.CalculateArea(10, 5);
+        Console.WriteLine("Rectangle Area: " + rectangleArea);
+        double circleArea = obj.CalculateArea(2.3);
+        Console.WriteLine("Circle Area: " + circleArea);
+    }
+
+}
