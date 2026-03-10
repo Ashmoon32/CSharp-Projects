9562f8ed53d6fae6ae56ba68e050fc62d01c3d46
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-10 18:15:07 +0630

    ex7

---
+++ Ch-3/ex7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 2;
+        int sum = 0;
+        while (i <= 20)
+        {
+            sum += i;
+            i += 2;
+        }
+        Console.WriteLine("The sum of even numbers from 1 to 20: " + sum);
+    }
+}
