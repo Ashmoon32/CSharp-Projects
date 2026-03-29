d300e71917b2d5125f481903dc0f522fab2eab45
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-29 15:01:03 +0630

    2

--- Practice-messy-dummy-blabla/Practice-messy-dummy-blabla/Program.cs
+++ Practice-messy-dummy-blabla/Practice-messy-dummy-blabla/Program.cs
@@ -11,11 +11,23 @@ class Program
     static void Main(string[] args)
     {
 
-        Student s1 = new Student();
-        s1.Name = "Ashmoon";
-        s1.Age = 21;
+        //Student s1 = new Student();
+        //s1.Name = "Ashmoon";
+        //s1.Age = 21;
 
-        Console.WriteLine("Name: " + s1.Name);
-        Console.WriteLine("Age: " + s1.Age);
+        //Console.WriteLine("Name: " + s1.Name);
+        //Console.WriteLine("Age: " + s1.Age);
+
+        //string StudentName = "Ashmoon";
+        //int[] Marks = { 90, 99, 98 };
+
+        //Console.WriteLine("Student Name: " + StudentName);
+        //Console.WriteLine("Marks: " + string.Join(",", Marks));
+
+        //string bookTitle = "Programming in C#";
+        //string[] authors = { "Mya Soe", "Soe Moe" };
+
+        //Console.WriteLine("Book Title: " + bookTitle);
+        //Console.WriteLine("Authors: " + string.Join(",", authors));
     }
 }
