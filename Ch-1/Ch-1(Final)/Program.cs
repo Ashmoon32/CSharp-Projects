90fee316d0adbf6f0d2056bf8fac47eb2b1c9fe9
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-29 10:50:34 +0630

    -

--- Ch-1/Ch-1(Final)/Program.cs
+++ Ch-1/Ch-1(Final)/Program.cs
@@ -4,55 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Good Day!");
+        //Console.WriteLine("Good Day!");
 
-        Console.Write("John ");
-        Console.WriteLine("Smith");
+        //Console.Write("John ");
+        //Console.WriteLine("Smith");
 
-        Console.WriteLine("1");
-        Console.WriteLine("101");
-        Console.WriteLine("1001");
+        //Console.WriteLine("1");
+        //Console.WriteLine("101");
+        //Console.WriteLine("1001");
 
-        DateTime Today = DateTime.Now;
-        Console.WriteLine(Today);
+        //DateTime Today = DateTime.Now;
+        //Console.WriteLine(Today);
 
-        Console.WriteLine(Today.ToString(
-            "dd-MM-yyyy"));
+        //Console.WriteLine(Today.ToString(
+        //    "dd-MM-yyyy"));
 
         
-        Console.WriteLine("Tomorrow: " + Today.AddDays(1).ToString("dddd, dd/MM/yyyy, HH:mm:ss"));
-        Console.WriteLine("Today: " + Today.ToString("dddd, dd/MM/yyyy, HH:mm:ss"));
-        Console.WriteLine("Yesterday: " + Today.AddDays(-1).ToString("dddd, dd/MM/yyyy, HH:mm:ss"));
+        //Console.WriteLine("Tomorrow: " + Today.AddDays(1).ToString("dddd, dd/MM/yyyy, HH:mm:ss"));
+        //Console.WriteLine("Today: " + Today.ToString("dddd, dd/MM/yyyy, HH:mm:ss"));
+        //Console.WriteLine("Yesterday: " + Today.AddDays(-1).ToString("dddd, dd/MM/yyyy, HH:mm:ss"));
 
-        double sqrt = Math.Sqrt(225);
-        double abs = Math.Abs(-1);
-        int min = Math.Min(10, 20);
-        Console.WriteLine(sqrt);
-        Console.WriteLine(abs);
-        Console.WriteLine(min);
-        Random rd = new Random();
-        int randomValue = rd.Next(1, 10);
-        Console.WriteLine(randomValue);
-        double deg = 120.1;
-        double rad = deg * Math.PI / 180;
-        Console.WriteLine($"Degree: {deg} is converted to Radian: {rad}.");
-        double radi = 3.1415;
-        double radToDeg = radi * 180 / Math.PI;
-        Console.WriteLine($"Radi: {radi} is converted to Deg: {radToDeg}.");
+        //double sqrt = Math.Sqrt(225);
+        //double abs = Math.Abs(-1);
+        //int min = Math.Min(10, 20);
+        //Console.WriteLine(sqrt);
+        //Console.WriteLine(abs);
+        //Console.WriteLine(min);
+        //Random rd = new Random();
+        //int randomValue = rd.Next(1, 10);
+        //Console.WriteLine(randomValue);
+        //double deg = 120.1;
+        //double rad = deg * Math.PI / 180;
+        //Console.WriteLine($"Degree: {deg} is converted to Radian: {rad}.");
+        //double radi = 3.1415;
+        //double radToDeg = radi * 180 / Math.PI;
+        //Console.WriteLine($"Radi: {radi} is converted to Deg: {radToDeg}.");
 
-        Console.Write("Enter your age: ");
-        int age = int.Parse(Console.ReadLine());
-        int newAge = age + 10;
-        Console.WriteLine("Your current age: " + age + ". Your age in next 10 year: " + newAge);
+        //Console.Write("Enter your age: ");
+        //int age = int.Parse(Console.ReadLine());
+        //int newAge = age + 10;
+        //Console.WriteLine("Your current age: " + age + ". Your age in next 10 year: " + newAge);
 
-        int i = 212;
-        float f = 123.1F;
-        char c = 'A';
-        bool b = false;
-        Console.WriteLine(i);
-        Console.WriteLine(f);
-        Console.WriteLine(c);
-        Console.WriteLine(b);
+        //int i = 212;
+        //float f = 123.1F;
+        //char c = 'A';
+        //bool b = false;
+        //Console.WriteLine(i);
+        //Console.WriteLine(f);
+        //Console.WriteLine(c);
+        //Console.WriteLine(b);
+
+        //string stu_Name = "Mg Mg";
+        //int stu_age = 20;
+        //char stu_grade = 'A';
+        //Console.WriteLine("Student Name: " + stu_Name);
+        //Console.WriteLine("Student Age: " + stu_age);
+        //Console.WriteLine("Student Grade: " + stu_grade);
 
 
     }
