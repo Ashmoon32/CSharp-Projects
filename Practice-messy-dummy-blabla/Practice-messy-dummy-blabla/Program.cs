650a19ffca9144194ea7c4ea22a2cb45211b17fc
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-29 13:27:01 +0630

    1

---
+++ Practice-messy-dummy-blabla/Practice-messy-dummy-blabla/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Student
+{
+    public string Name;
+    public int Age;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Student s1 = new Student();
+        s1.Name = "Ashmoon";
+        s1.Age = 21;
+
+        Console.WriteLine("Name: " + s1.Name);
+        Console.WriteLine("Age: " + s1.Age);
+    }
+}
