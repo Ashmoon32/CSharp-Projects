f5a430f28ffd30fe776daefb8b6774c352d1653a
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-29 09:43:32 +0630

    -

--- Ch-1/Ch-1(Final)/Program.cs
+++ Ch-1/Ch-1(Final)/Program.cs
@@ -2,11 +2,28 @@
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         Console.WriteLine("Good Day!");
 
         Console.Write("John ");
         Console.WriteLine("Smith");
+
+        Console.WriteLine("1");
+        Console.WriteLine("101");
+        Console.WriteLine("1001");
+
+        DateTime Today = DateTime.Now;
+        Console.WriteLine(Today);
+
+        Console.WriteLine(Today.ToString(
+            "dd-MM-yyyy"));
+
+        
+        Console.WriteLine("Tomorrow: " + Today.AddDays(1).ToString("dddd, dd/MM/yyyy, HH:mm:ss"));
+        Console.WriteLine("Today: " + Today.ToString("dddd, dd/MM/yyyy, HH:mm:ss"));
+        Console.WriteLine("Yesterday: " + Today.AddDays(-1).ToString("dddd, dd/MM/yyyy, HH:mm:ss"));
+
+
     }
 }
