deb85f3d6ab554b06dc882749c32603ea959fa89
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-29 15:12:55 +0630

    3

--- Practice-messy-dummy-blabla/Practice-messy-dummy-blabla/Program.cs
+++ Practice-messy-dummy-blabla/Practice-messy-dummy-blabla/Program.cs
@@ -29,5 +29,28 @@ class Program
 
         //Console.WriteLine("Book Title: " + bookTitle);
         //Console.WriteLine("Authors: " + string.Join(",", authors));
+
+        //Student[] students = new Student[2];
+
+        //students[0] = new Student();
+        //students[0].Name = "Ash";
+        //students[0].Age = 20;
+
+        //students[1] = new Student();
+        //students[1].Name = "Moon";
+        //students[1].Age = 21;
+
+        //foreach( Student s in students)
+        //{
+        //    Console.WriteLine("Name : " + s.Name + ", Age : " + s.Age);
+        //}
+
+        int? age = null;
+        bool? isGraduated = null;
+
+        age = 21;
+
+        Console.WriteLine("Age: " + (age.HasValue ? age.ToString() : "No value"));
+        Console.WriteLine("Graduated: " + (isGraduated.HasValue ? isGraduated.ToString() : "No value"));
     }
 }
