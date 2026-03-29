c7b1fd22cd41683447ef2dda085d16984a088583
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-29 09:19:32 +0630

    -

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+struct Employee
+{
+    public int ID;
+public string Name;
+public double salary;
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee emp;
+        emp.ID = 102;
+        emp.Name = "Dick";
+        emp.salary = 3000000;
+        Console.WriteLine("Employee ID: " + emp.ID);
+        Console.WriteLine("Employee Name: " + emp.Name);
+        Console.WriteLine("Employee Salary: " + emp.salary);
+    }
+}
